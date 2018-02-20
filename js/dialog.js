79a1f4ca403fb83a51ebe1952f9d6335af0108c8
Indiel <kori.yanagi@gmail.com>
2018-02-20 14:51:27 +0200

    Добавлена возможность перетаскивания предметов из магазина в рюкзак

--- js/dialog.js
+++ js/dialog.js
@@ -19,9 +19,18 @@
   var closePopup = function () {
     setup.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
+
+    setup.style.left = START_COORDS.x;
+    setup.style.top = START_COORDS.y;
   };
 
-  setupOpen.addEventListener('click', openPopup);
+  setupOpen.addEventListener('click', function () {
+    if (!setup.classList.contains('hidden')) {
+      closePopup();
+    } else {
+      openPopup();
+    }
+  });
 
   setupOpen.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, closePopup);
@@ -31,6 +40,62 @@
 
   setupClose.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, closePopup);
+  });
+
+  // Перетаскивание диалога
+  var dialogHandle = setup.querySelector('.upload');
+
+  var START_COORDS = {
+    x: setup.style.left,
+    y: setup.style.top
+  };
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandle.removeEventListener('click', onClickPreventDefault);
+        };
+
+        dialogHandle.addEventListener('click', onClickPreventDefault);
+      }
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 
 })();
--- js/setup.js
+++ js/setup.js
@@ -43,4 +43,58 @@
 
   similarListElement.appendChild(fragment);
 
+  // Перетаскивание предметов
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var artifactsElement = document.querySelector('.setup-artifacts');
+  var draggedElement;
+  var copyDraggedElement;
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName === 'IMG') {
+      draggedElement = evt.target;
+      copyDraggedElement = draggedElement.cloneNode();
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+      artifactsElement.style.outline = '2px dashed red';
+    }
+  });
+
+  artifactsElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName === 'IMG') {
+      copyDraggedElement = evt.target;
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+      artifactsElement.style.outline = '2px dashed red';
+    }
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    if (evt.target.childNodes.length !== 1 && evt.target.tagName !== 'IMG') {
+      evt.preventDefault();
+    }
+    artifactsElement.style.outline = '2px dashed red';
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.appendChild(copyDraggedElement);
+    evt.target.style.backgroundColor = '';
+    artifactsElement.style.outline = '';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    if (evt.target.childNodes.length === 1) {
+      evt.target.style.backgroundColor = 'red';
+    } else if (evt.target.tagName === 'IMG') {
+      evt.target.parentNode.style.backgroundColor = 'red';
+    } else {
+      evt.target.style.backgroundColor = 'yellow';
+    }
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.target.parentNode.style.backgroundColor = '';
+    evt.preventDefault();
+  });
+
 })();
